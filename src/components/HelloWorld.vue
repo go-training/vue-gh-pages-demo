c2134c60090f76fe3a82f04191fd8c3e2cd64f2e
Bo-Yi Wu <appleboy.tw@gmail.com>
2020-02-28 11:12:53 +0800

    chore: test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -42,7 +42,7 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
-  margin: 40px 0 0;
+  margin: 50px 0 0;
 }
 ul {
   list-style-type: none;
